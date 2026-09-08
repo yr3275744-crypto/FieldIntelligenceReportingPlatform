7378eb7fec54bf4fb9d435de6984dda549c94f62
Ytshak_Raiyner <yr3275744@gmail.com>
2026-09-08 15:53:22 +0300

    first version of producer

---
+++ services/producer/configuration.py
@@ -0,0 +1,23 @@
+from confluent_kafka import Producer
+import socket
+import os
+from pathlib import Path
+import logging
+
+conf = {'bootstrap.servers': os.environ["KAFKA_SERVERS"],
+        'client.id': socket.gethostname()}
+
+producer = Producer(conf)
+
+topic = os.environ["KAFKA_TOPIC"]
+
+logs_file_path = Path(__file__).parents[2] / "logs" / "logs.log"
+
+file_handler = logging.FileHandler(logs_file_path)
+logger = logging.basicConfig(handlers=[file_handler])
+logger = logging.getLogger("producer-logger")
+
+try:
+    data_file_path = Path(__file__).parents[2] / "data" / "field_reports.json"
+except FileNotFoundError as ex:
+        logger.error(f"file not found: {ex.filename}")
---
+++ services/producer/main.py
@@ -0,0 +1,22 @@
+import configuration;
+import json
+import logging
+logger = logging.getLogger()
+
+def acked(err, msg):
+    if err is not None:
+        print("Failed to deliver message: %s: %s" % (str(msg), str(err)))
+    else:
+        print("Message produced: %s" % (str(msg)))
+
+def main(path):
+        with open(path, "r") as file:
+            data = json.load(file)
+        for val in data:
+            configuration.producer.produce(configuration.topic, 
+                                           value=json.dumps(val),
+                                           callback=acked)
+            configuration.producer.poll(0.5)
+
+if __name__ == "__main__":
+    main(configuration.data_file_path)
