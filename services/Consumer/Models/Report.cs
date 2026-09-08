e989c4ecae182bb84d7434811ab1ed1b2e0dc53d
Ytshak_Raiyner <yr3275744@gmail.com>
2026-09-08 16:40:51 +0300

    add the model

---
+++ services/Consumer/Models/Report.cs
@@ -0,0 +1,26 @@
+﻿using Consumer.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumer.Models
+{
+    public class Report
+    {
+        public string ReportId { get; set; } = string.Empty;
+        public DateTime Timestamp { get; set; }
+        public string AgentId { get; set; } = string.Empty;
+        public string Unit { get; set; } = string.Empty;
+        public string Theater { get; set; } = string.Empty;
+        public string Sector { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public ValidReportTypes ReportType { get; set; }
+        public ValidPriorities Priority { get; set; }
+        public string SourceType { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public string? SubjectId { get; set; }
+        public string? SubjectType { get; set; }
+    }
+}
